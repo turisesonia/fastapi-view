fc6ed411d11a3bb5495ac80ee18b51402d85142b
Sam Yao <turisesonia@gmail.com>
2022-02-14 15:08:35 +0800

    feat: inertia add share data, partial reload feature

--- fastapi_view/inertia.py
+++ fastapi_view/inertia.py
@@ -1,5 +1,7 @@
 import ujson
 import hashlib
+from typing import Any
+
 from fastapi import Request
 from fastapi.responses import JSONResponse
 from . import view
@@ -8,6 +10,7 @@ from . import view
 class _Inertia(object):
     def __init__(self):
         self._root_path = "app"
+        self._share_data = {}
 
     @property
     def root_path(self):
@@ -17,6 +20,18 @@ class _Inertia(object):
     @root_path.setter
     def root_path(self, root_path: str):
         self._root_path = root_path
+
+    @property
+    def share_data(self):
+        """The share_data property."""
+        return self._share_data
+
+    @share_data.setter
+    def share_data(self, share_data: dict):
+        self._share_data = share_data
+
+    def share(self, key: str, value: Any):
+        self._share_data[key] = value
 
     def get_inertia_version(self):
         m = (
@@ -31,6 +46,15 @@ class _Inertia(object):
         return hashlib.sha256(bytes_content).hexdigest()
 
     def render(self, component: str, request: Request, props: dict = {}):
+        props = {**self.share_data, **props}
+
+        partial_props = request.headers.getlist("X-Inertia-Partial-Data")
+
+        if partial_props and component == request.headers.get(
+            "X-Inertia-Partial-Component"
+        ):
+            props = {key: value for key, value in props.items() if key in partial_props}
+
         page = {
             "version": self.get_inertia_version(),
             "component": component,
