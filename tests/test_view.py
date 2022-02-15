b8fd41211f4db9b66d25067767e5d067c3132fda
Sam Yao <turisesonia@gmail.com>
2022-02-15 10:28:27 +0800

    refactor: change test name and refine test

--- tests/test_view.py
+++ tests/test_view.py
@@ -2,16 +2,12 @@ import os
 
 from fastapi import FastAPI, Request
 from fastapi.testclient import TestClient
-from fastapi.staticfiles import StaticFiles
+
 
 from fastapi_view import view, inertia
 
 app_name = "Test fastapi app"
-
 app = FastAPI(title=app_name)
-
-
-view.views_directory = f"{os.path.abspath('tests')}/resources/views"
 
 
 @app.get("/")
@@ -24,10 +20,14 @@ def inertia_index(request: Request):
     return inertia.render("Index", request=request, props={"name": "Index"})
 
 
+view.views_directory = f"{os.path.abspath('tests')}/resources/views"
+
 client = TestClient(app)
 
 
 def test_view_response():
     response = client.get("/")
 
+    assert response.ok
+    assert response.headers["content-type"] == "text/html; charset=utf-8"
     assert response.template.name == "index.html"
