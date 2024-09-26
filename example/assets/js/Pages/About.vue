15642cdfd5a43e0b274b3736eb354f9960cc0cd5
Sam Yao <turisesonia@gmail.com>
2024-09-26 15:33:36 +0800

    docs: add example app

---
+++ example/assets/js/Pages/About.vue
@@ -0,0 +1,10 @@
+<script setup>
+const props = defineProps({})
+</script>
+
+<template>
+  <div>
+    <h1>About</h1>
+    <p>This is the about page</p>
+  </div>
+</template>
---
+++ example/assets/js/Pages/Index.vue
@@ -0,0 +1,9 @@
+<script setup>
+const props = defineProps({})
+</script>
+
+<template>
+  <div>
+    <h1>Welcome to your Inertia app!</h1>
+  </div>
+</template>
